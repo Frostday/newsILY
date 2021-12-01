dd7f7acb8a830490c1ecfa10aeeb1c1429084f6f
Yuvraj04 <yuvrajg942@gmail.com>
2021-12-01 15:37:48 +0530

    update

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, render_template, redirect, url_for
 import requests
+import sqlite3
+import os
+currentlocation=os.path.dirname(os.path.abspath(__file__))
+
 from datetime import date
 
 app = Flask(__name__)
@@ -20,5 +24,31 @@ def index():
     articles = response.json()['articles']
 
     return render_template('./index.html', data=articles)
+@app.route('/', methods=['POST'])
+def checklogin():
+    UN=request.form['Username']
+    PW=request.form['Password']
+    sqlconnection=sqlite3.Connection(currentlocation + "/login.db")
+    cursor=sqlconnection.cursor()
+    query1="SELECT Username,Password From Users WHERE Username={UN} AND Password={PW})"
+    rows=cursor.execute(query1)
+    rows=rows.fetchall()
+    if len(rows) == 1:
+        return render_template("index.html")
+    else :
+        return redirect("/register")
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method=="POST":
+        dUN=request.form[' Enter Your sername']
+        email=request.form['Enter Email ID']
+        dPW=request.form[' Enter Password']
+        sqlconnection=sqlite3.Connection(currentlocation + "/login.db")
+        cursor=sqlconnection.cursor()
+        query1="INSERT INTO Users VALUES('{u}','{p}','{ue}')".format(u=dUN,p=dPW,ue=email)
+        cursor.execute(query1)
+        sqlconnection.commit()
+        return redirect("/")
+    return render_template("Register.html")
 
 app.run(debug=False, port=8080)
