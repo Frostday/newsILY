0cea01333c11b5989082ab9b240eedafbab62373
FrostDayee <dhruvgarg01@gmail.com>
2021-12-02 15:10:51 +0530

    update

--- main.py
+++ main.py
@@ -21,37 +21,51 @@ searched = False
 def index():
     global logged_in, user_email, user_name, searched
     if request.method == "POST":
-        searched = True
-        # print(request.form)
-        keywords = request.form['article-keywords-phrase']
-        language = request.form['language']
-        # (YYYY-MM-DD)
-        search_from = request.form['search-from']
-        search_to = request.form['search-to']
-        if request.form.get('dropdown-menu'):
-            sorting = request.form['dropdown-menu']
-        else:
-            sorting = 'relevancy'
+        if request.form.get('logout-button'):
+            logged_in = False
+            user_email = ''
+            user_name = ''
+            searched = False
+            searched = False
+            url = (f'https://newsapi.org/v2/top-headlines?'
+                'category=general&'
+                'language=en&'
+                f'apiKey={API_KEY}')
+            response = requests.get(url)
+            articles = response.json()['articles']
+            return render_template('./index.html', data=articles, x=logged_in, y=user_name, z=searched)
 
-        url = 'https://newsapi.org/v2/everything?'
-        if keywords != '':
-            url += 'q=' + keywords + '&'
-        if language != '':
-            url += 'language=' + language + '&'
-        else:
-            url += 'language=en&'
-        if search_from != '':
-            url += 'from=' + search_from + '&'
-        if search_to != '':
-            url += 'to=' + search_to + '&'
-        if sorting != '':
-            url += 'sortBy=' + sorting + '&'
-        url += 'apiKey=' + API_KEY
+        elif request.form.get('submit-button-1'):
+            searched = True
+            keywords = request.form['article-keywords-phrase']
+            language = request.form['language']
+            # (YYYY-MM-DD)
+            search_from = request.form['search-from']
+            search_to = request.form['search-to']
+            if request.form.get('dropdown-menu'):
+                sorting = request.form['dropdown-menu']
+            else:
+                sorting = 'relevancy'
 
-        response = requests.get(url)
-        # print(response.json())
-        articles = response.json()['articles']
-        return render_template('./index.html', data=articles, x=logged_in, y=user_name, z=searched)
+            url = 'https://newsapi.org/v2/everything?'
+            if keywords != '':
+                url += 'q=' + keywords + '&'
+            if language != '':
+                url += 'language=' + language + '&'
+            else:
+                url += 'language=en&'
+            if search_from != '':
+                url += 'from=' + search_from + '&'
+            if search_to != '':
+                url += 'to=' + search_to + '&'
+            if sorting != '':
+                url += 'sortBy=' + sorting + '&'
+            url += 'apiKey=' + API_KEY
+
+            response = requests.get(url)
+            # print(response.json())
+            articles = response.json()['articles']
+            return render_template('./index.html', data=articles, x=logged_in, y=user_name, z=searched)
     else:
         searched = False
         url = (f'https://newsapi.org/v2/top-headlines?'
@@ -59,8 +73,6 @@ def index():
                'language=en&'
                f'apiKey={API_KEY}')
         response = requests.get(url)
-        # print(response.json())
-        # print(response.json()['articles'][0]['content'])
         articles = response.json()['articles']
 
         return render_template('./index.html', data=articles, x=logged_in, y=user_name, z=searched)
@@ -116,7 +128,7 @@ def shared():
     query = f'SELECT email_1, name_1, article_info from Shared WHERE email_2="{user_email}"'
     rows = cursor.execute(query)
     rows = rows.fetchall()
-    print(rows)
+    # print(rows)
     rows = [list(i) for i in rows]
     for i in range(len(rows)):
         rows[i][2] = eval(rows[i][2])
@@ -126,9 +138,9 @@ def shared():
 @app.route('/article', methods=['GET', 'POST'])
 def article_info():
     global logged_in, user_email, user_name
-    print(request.form)
+    # print(request.form)
     if request.form.get('article-info'):
-        print(request.form['article-info'])
+        # print(request.form['article-info'])
         info = eval(request.form['article-info'])
     else:
         info = eval(request.form['submit-button'])
@@ -144,7 +156,7 @@ def article_info():
         shared_to = request.form['shared-to']
         sqlconnection = sqlite3.Connection(currentlocation + "/shared.db")
         cursor = sqlconnection.cursor()
-        print(info)
+        # print(info)
         for i in info.keys():
             if type(info[i]) == str and info[i] != None:
                 info[i] = info[i].replace("'", " ")
@@ -155,7 +167,7 @@ def article_info():
                         info[i][j] = info[i][j].replace("'", " ")
                         info[i][j] = info[i][j].replace('"', ' ')
         query = f'INSERT INTO Shared VALUES("{user_email}", "{user_name}", "{shared_to}", "{info}")'
-        print(query)
+        # print(query)
         cursor.execute(query)
         sqlconnection.commit()
     return render_template("./article.html", x=logged_in, y=user_name, info=info)
